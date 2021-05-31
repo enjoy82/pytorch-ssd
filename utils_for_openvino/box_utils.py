a21835ba9c3b4c3f8977ee74cc011cb008c17a3e
enjoy82 <naoya990506@gmail.com>
2021-06-01 00:06:38 +0900

    for raspi

--- utils_for_openvino/box_utils.py
+++ utils_for_openvino/box_utils.py
@@ -1,7 +1,5 @@
-import torch
-import itertools
+import numpy as np
 from typing import List
-import math
 
 def area_of(left_top, right_bottom) -> torch.Tensor:
     """Compute the areas of rectangles given two corners.
@@ -13,7 +11,7 @@ def area_of(left_top, right_bottom) -> torch.Tensor:
     Returns:
         area (N): return the area.
     """
-    hw = torch.clamp(right_bottom - left_top, min=0.0)
+    hw = np.clip(right_bottom - left_top, min=0.0)
     return hw[..., 0] * hw[..., 1]
 
 
@@ -27,50 +25,13 @@ def iou_of(boxes0, boxes1, eps=1e-5):
     Returns:
         iou (N): IoU values.
     """
-    overlap_left_top = torch.max(boxes0[..., :2], boxes1[..., :2])
-    overlap_right_bottom = torch.min(boxes0[..., 2:], boxes1[..., 2:])
+    overlap_left_top = np.amax(boxes0[..., :2], boxes1[..., :2])
+    overlap_right_bottom = np.amin(boxes0[..., 2:], boxes1[..., 2:])
 
     overlap_area = area_of(overlap_left_top, overlap_right_bottom)
     area0 = area_of(boxes0[..., :2], boxes0[..., 2:])
     area1 = area_of(boxes1[..., :2], boxes1[..., 2:])
     return overlap_area / (area0 + area1 - overlap_area + eps)
-
-
-def hard_negative_mining(loss, labels, neg_pos_ratio):
-    """
-    It used to suppress the presence of a large number of negative prediction.
-    It works on image level not batch level.
-    For any example/image, it keeps all the positive predictions and
-     cut the number of negative predictions to make sure the ratio
-     between the negative examples and positive examples is no more
-     the given ratio for an image.
-
-    Args:
-        loss (N, num_priors): the loss for each example.
-        labels (N, num_priors): the labels.
-        neg_pos_ratio:  the ratio between the negative examples and positive examples.
-    """
-    pos_mask = labels > 0
-    num_pos = pos_mask.long().sum(dim=1, keepdim=True)
-    num_neg = num_pos * neg_pos_ratio
-
-    loss[pos_mask] = -math.inf
-    _, indexes = loss.sort(dim=1, descending=True)
-    _, orders = indexes.sort(dim=1)
-    neg_mask = orders < num_neg
-    return pos_mask | neg_mask
-
-
-def center_form_to_corner_form(locations):
-    return torch.cat([locations[..., :2] - locations[..., 2:]/2,
-                     locations[..., :2] + locations[..., 2:]/2], locations.dim() - 1) 
-
-
-def corner_form_to_center_form(boxes):
-    return torch.cat([
-        (boxes[..., :2] + boxes[..., 2:]) / 2,
-         boxes[..., 2:] - boxes[..., :2]
-    ], boxes.dim() - 1)
 
 
 def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
@@ -108,45 +69,8 @@ def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
 
 def nms(box_scores, nms_method=None, score_threshold=None, iou_threshold=None,
         sigma=0.5, top_k=-1, candidate_size=200):
+        #TODO
     if nms_method == "soft":
-        return soft_nms(box_scores, score_threshold, sigma, top_k)
+        return hard_nms(box_scores, score_threshold, sigma, top_k)
     else:
         return hard_nms(box_scores, iou_threshold, top_k, candidate_size=candidate_size)
-
-
-def soft_nms(box_scores, score_threshold, sigma=0.5, top_k=-1):
-    """Soft NMS implementation.
-
-    References:
-        https://arxiv.org/abs/1704.04503
-        https://github.com/facebookresearch/Detectron/blob/master/detectron/utils/cython_nms.pyx
-
-    Args:
-        box_scores (N, 5): boxes in corner-form and probabilities.
-        score_threshold: boxes with scores less than value are not considered.
-        sigma: the parameter in score re-computation.
-            scores[i] = scores[i] * exp(-(iou_i)^2 / simga)
-        top_k: keep top_k results. If k <= 0, keep all the results.
-    Returns:
-         picked_box_scores (K, 5): results of NMS.
-    """
-    picked_box_scores = []
-    while box_scores.size(0) > 0:
-        max_score_index = torch.argmax(box_scores[:, 4])
-        cur_box_prob = torch.tensor(box_scores[max_score_index, :])
-        picked_box_scores.append(cur_box_prob)
-        if len(picked_box_scores) == top_k > 0 or box_scores.size(0) == 1:
-            break
-        cur_box = cur_box_prob[:-1]
-        box_scores[max_score_index, :] = box_scores[-1, :]
-        box_scores = box_scores[:-1, :]
-        ious = iou_of(cur_box.unsqueeze(0), box_scores[:, :-1])
-        box_scores[:, -1] = box_scores[:, -1] * torch.exp(-(ious * ious) / sigma)
-        box_scores = box_scores[box_scores[:, -1] > score_threshold, :]
-    if len(picked_box_scores) > 0:
-        return torch.stack(picked_box_scores)
-    else:
-        return torch.tensor([])
-
-
-
