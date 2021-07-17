2dc52f323b4cfbebcbb54653b50d33016c736550
enjoy82 <naoya990506@gmail.com>
2021-07-18 01:55:15 +0900

    add

--- live_demo_cpp/live_demo_forcpp.cpp
+++ live_demo_cpp/live_demo_forcpp.cpp
@@ -233,7 +233,7 @@ int main(){
     InferRequest::Ptr async_infer_request;
     std::string input_name;
     std::string output_name;
-    std::string device = "CPU";
+    std::string device = "MYRIAD";
     //std::string device = "MYRIAD";
     std::string modelPath = "./models/mbv3-ssd-cornv1.xml";
     int result = 0;
@@ -244,6 +244,7 @@ int main(){
     double fps = 30.0;
     double width = 640.0;
     double height = 480.0;
+    float threshold = 0.5;
     cv::VideoCapture cap(cv::CAP_DSHOW + camera_id);
     if(!cap.isOpened()){ //エラー処理
 		 std::cout << "cap error" << std::endl;
@@ -262,6 +263,9 @@ int main(){
     LoadModel(network, plugin, executable_network);
     CreateInferRequest(executable_network, async_infer_request);
 
+    const int numDetections = outputDims[2];
+    const int objectSize = outputDims[3];
+
     while(cap.read(frame)){
         cv::imshow("frame", frame);
         int key = cv::waitKey(1);
@@ -270,10 +274,35 @@ int main(){
 			cv::destroyWindow("frame");
 			break;
         }
-        /*
+        
         PrepareInput(async_infer_request, input_name, frame);
         Infer(async_infer_request);
-        result = ProcessOutput(async_infer_request, output_name);
-        */
+        //result = ProcessOutput(async_infer_request, output_name);
+        cv::putText(frame, "test", cv::Point2f(0, 20), cv::FONT_HERSHEY_TRIPLEX, 0.6, cv::Scalar(0, 0, 255));
+        const float *detections = infer_request->GetBlob(outputName)->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+        //TODO class classification
+        for (int i = 0; i < numDetections; i++) {
+            float confidence = detections[i * objectSize + 2];
+            float xmin = detections[i * objectSize + 3] * width;
+            float ymin = detections[i * objectSize + 4] * height;
+            float xmax = detections[i * objectSize + 5] * width;
+            float ymax = detections[i * objectSize + 6] * height;
+
+            if (confidence > threshold) {
+                std::ostringstream conf;
+                conf << std::fixed << std::setprecision(3) << confidence;
+                cv::putText(frame,
+                    conf.str(),
+                    cv::Point2f(xmin, ymin - 5), cv::FONT_HERSHEY_COMPLEX_SMALL, 1,
+                    cv::Scalar(0, 0, 255));
+                cv::rectangle(frame, cv::Point2f(xmin, ymin), cv::Point2f(xmax, ymax), cv::Scalar(0, 0, 255));
+            }
+        }
+
+        cv::imshow("object Detector", frame);
+
+        const int key = cv::waitKey(1);
+            if (27 == key)  // Esc
+                break;
     }
 }
